4786fe75933d7bd4bf925358ad302cece7be1df4
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2024-06-04 07:44:24 +0530

    header and admin dashboard update

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+
+class DashboardController extends Controller
+{
+     public function viewdashboard(){
+        return view('admin.user.dashboard');
+     }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//admin dashboard route
+Route::get('/admin/dashboard', [DashboardController::class, 'viewdashboard'])->name('admindashboard');
