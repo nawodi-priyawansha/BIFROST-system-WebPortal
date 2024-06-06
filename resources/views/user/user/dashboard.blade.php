82cd4bddd4521b50c629234771fd21d00786a86c
Sajith <sajithniroj@gmail.com>
2024-06-06 10:32:29 +0530

    Created Full User Dashboard UI

--- resources/views/user/user/dashboard.blade.php
+++ resources/views/user/user/dashboard.blade.php
@@ -24,9 +24,9 @@
             </div>
         </div>
         <div class="container mx-auto px-2">
-            <div class="sm:flex flex-wrap -mx-2">
+            <div class="flex flex-wrap -mx-2">
                 <!-- Profile Section -->
-                <div class="sm:w-full md:w-1/4 px-1 mb-4">
+                <div class="w-full md:w-1/4 px-1 mb-4">
                     <div class="bg-white shadow rounded-lg p-6">
                         <div class="flex justify-between">
                             <a href="#" class="btn p-2 rounded-lg text-white bg-black mb-4">Profile</a>
@@ -34,9 +34,9 @@
                         </div>
                         <div class="flex items-center mt-4 space-x-4">
                             <img src="{{ asset('img/valhalla-bg.jpg') }}" alt="Profile Picture" class="w-20 h-20 rounded-full">
-                            <div class="flex flex-col ">
+                            <div class="flex flex-col">
                                 <h5 class="text-2xl font-semibold whitespace-nowrap overflow-hidden text-ellipsis">Richard Jones</h5>
-                                <h6 class="text-gray-500 text-lg ">Male, 28 years</h6>
+                                <h6 class="text-gray-500 text-lg">Male, 28 years</h6>
                             </div>
                         </div>
                         <div class="flex justify-between border-t border-black items-center mt-6">
@@ -53,7 +53,7 @@
                     </div>
                 </div>
                 <!-- Workout Section -->
-                <div class="sm:w-full md:w-3/4 px-2">
+                <div class="w-full md:w-3/4 px-2 mb-4">
                     <div class="bg-white shadow rounded-lg">
                         <div class="flex py-2">
                             <label for="text" class="bg-red-600 mt-4 pr-4 pl-4 rounded-md ml-4 text-lg p-1 text-white">Workout - Deadlift</label>
@@ -71,18 +71,18 @@
                                         <td class="py-2 pb-4"></td>
                                     </tr>
                                     <tr>
-                                    <td class="py-2 pb-4">Strength</td>
-                                       <td class="py-2 pb-4">2 x 10 bar only</td>
-                                       <td class="py-2 pb-4 justify-end  flex sm:flex-row flex-row ">
-                                           <input type="text" class="form-input border rounded-md border-black mr-3 w-full sm:w-24">
-                                           <button class="btn btn-outline-secondary hover:text-yellow-600 mt-2 sm:mt-0">Save</button>
-                                       </td>
-
+                                        <td class="py-2 pb-4">Strength</td>
+                                        <td class="py-2 pb-4">2 x 10 bar only</td>
+                                        <td class="py-2 pb-4 justify-end flex  sm:flex-row">
+                                            <input type="text" class="form-input border rounded-md border-black mr-3 w-full sm:w-24">
+                                            <button class="btn btn-outline-secondary hover:text-yellow-600 mt-2 sm:mt-0">Save</button>
+                                        </td>
+                                    </tr>
                                     </tr>
                                     <tr class="border-b border-black">
                                         <td class="py-2 pb-4"></td>
                                         <td class="py-2 pb-4">2 x 10 bar only</td>
-                                        <td class="py-2 pb-4 justify-end  flex sm:flex-row flex-row ">
+                                        <td class="py-2 pb-4 justify-end flex  sm:flex-row">
                                             <input type="text" class="form-input border rounded-md border-black mr-3 w-full sm:w-24">
                                             <button class="btn btn-outline-secondary hover:text-yellow-600 mt-2 sm:mt-0">Save</button>
                                         </td>
@@ -95,7 +95,7 @@
                                     <tr>
                                         <td class="py-2 pb-4"></td>
                                         <td class="pb-4 py-2">- 10 Goblet Squat</td>
-                                        <td class="py-2 pb-4 justify-end  flex sm:flex-row flex-row ">
+                                        <td class="py-2 pb-4 justify-end flex  sm:flex-row">
                                             <input type="text" class="form-input border rounded-md border-black mr-3 w-full sm:w-24">
                                             <button class="btn btn-outline-secondary hover:text-yellow-600 mt-2 sm:mt-0">Save</button>
                                         </td>
@@ -103,7 +103,7 @@
                                     <tr>
                                         <td class="py-2 pb-4"></td>
                                         <td class="py-2 pb-4">- 60 Sec Plank</td>
-                                        <td class="py-2 pb-4 justify-end  flex sm:flex-row flex-row ">
+                                        <td class="py-2 pb-4 justify-end flex  sm:flex-row">
                                             <input type="text" class="form-input border rounded-md border-black mr-3 w-full sm:w-24">
                                             <button class="btn btn-outline-secondary hover:text-yellow-600 mt-2 sm:mt-0">Save</button>
                                         </td>
@@ -111,7 +111,7 @@
                                     <tr>
                                         <td class="py-2 pb-4"></td>
                                         <td class="py-2 pb-4">- 10 Single Arm bent over row</td>
-                                        <td class="py-2 pb-4 justify-end  flex sm:flex-row flex-row ">
+                                        <td class="py-2 pb-4 justify-end flex  sm:flex-row">
                                             <input type="text" class="form-input border rounded-md border-black mr-3 w-full sm:w-24">
                                             <button class="btn btn-outline-secondary hover:text-yellow-600 mt-2 sm:mt-0">Save</button>
                                         </td>
@@ -120,90 +120,101 @@
                             </table>
                         </div>
                     </div>
-                    <div style="width: 75%; margin: auto;">
-                        <canvas id="myChart"></canvas>
-                    </div>
-                    <script>
-                        var ctx = document.getElementById('myChart').getContext('2d');
-                        var myChart = new Chart(ctx, {
-                            type: 'line',
-                            data: {
-                                labels: ['2018', '2019', '2020', '2021', '2022', '2023'],
-                                datasets: [
-                                    {
-                                        label: 'Weight',
-                                        data: [1000, 1200, 800, 1500, 900, 1750],
-                                        borderColor: 'rgba(75, 192, 192, 1)',
-                                        borderWidth: 2,
-                                        fill: false,
-                                        tension: 0.4
-                                    },
-                                    {
-                                        label: 'Height',
-                                        data: [500, 700, 600, 800, 500, 1200],
-                                        borderColor: 'rgba(255, 99, 132, 1)',
-                                        borderWidth: 2,
-                                        fill: false,
-                                        tension: 0.4
-                                    },
-                                    {
-                                        label: 'Heading 1',
-                                        data: [800, 600, 1200, 900, 1300, 500],
-                                        borderColor: 'rgba(54, 162, 235, 1)',
-                                        borderWidth: 2,
-                                        fill: false,
-                                        tension: 0.4
-                                    },
-                                    {
-                                        label: 'Heading 2',
-                                        data: [600, 900, 500, 700, 800, 1500],
-                                        borderColor: 'rgba(255, 159, 64, 1)',
-                                        borderWidth: 2,
-                                        fill: false,
-                                        tension: 0.4
-                                    }
-                                ]
-                            },
-                            options: {
-                                responsive: true,
-                                scales: {
-                                    y: {
-                                        beginAtZero: true
-                                    }
+                </div>
+                <!-- Statistics Section -->
+                <div class="md:w-1/4">
+                </div>
+                <div class="w-full md:w-3/4 px-2 mb-4 ">
+                    <div class="bg-white shadow rounded-lg ">
+                        <div class="flex py-2 bg-white sm:mb-4">
+                            <label for="text" class="bg-red-600 mt-4 pr-4 pl-4 rounded-md ml-4 text-lg p-1 text-white">Statistics - Workout History</label>
+                        </div>
+                        <div class="pt-6 pb-14 w-full ">
+                            <canvas id="myChart" class="sm:h-96 h-80"></canvas>
+                        </div>
+                        <script>
+                            var ctx = document.getElementById('myChart').getContext('2d');
+                            var myChart = new Chart(ctx, {
+                                type: 'line',
+                                data: {
+                                    labels: ['2018', '2019', '2020', '2021', '2022', '2023'],
+                                    datasets: [
+                                        {
+                                            label: 'Weight',
+                                            data: [1000, 1200, 800, 1500, 900, 1750],
+                                            borderColor: 'rgba(75, 192, 192, 1)',
+                                            borderWidth: 2,
+                                            fill: false,
+                                            tension: 0.4
+                                        },
+                                        {
+                                            label: 'Height',
+                                            data: [500, 700, 600, 800, 500, 1200],
+                                            borderColor: 'rgba(255, 99, 132, 1)',
+                                            borderWidth: 2,
+                                            fill: false,
+                                            tension: 0.4
+                                        },
+                                        {
+                                            label: 'Heading 1',
+                                            data: [800, 600, 1200, 900, 1300, 500],
+                                            borderColor: 'rgba(54, 162, 235, 1)',
+                                            borderWidth: 2,
+                                            fill: false,
+                                            tension: 0.4
+                                        },
+                                        {
+                                            label: 'Heading 2',
+                                            data: [600, 900, 500, 700, 800, 1500],
+                                            borderColor: 'rgba(255, 159, 64, 1)',
+                                            borderWidth: 2,
+                                            fill: false,
+                                            tension: 0.4
+                                        }
+                                    ]
                                 },
-                                plugins: {
-                                    annotation: {
-                                        annotations: [
-                                            {
-                                                type: 'point',
-                                                xValue: '2021',
-                                                yValue: 1500,
-                                                backgroundColor: 'rgba(0, 255, 0, 0.5)',
-                                                borderColor: 'rgb(0, 255, 0)',
-                                                borderWidth: 2,
-                                                radius: 5,
-                                            },
-                                            {
-                                                type: 'point',
-                                                xValue: '2023',
-                                                yValue: 1200,
-                                                backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                                                borderColor: 'rgb(255, 0, 0)',
-                                                borderWidth: 2,
-                                                radius: 5,
-                                            }
-                                        ]
+                                options: {
+                                    responsive: true,
+                                    maintainAspectRatio: false,
+                                    scales: {
+                                        y: {
+                                            beginAtZero: true
+                                        }
+                                    },
+                                    plugins: {
+                                        annotation: {
+                                            annotations: [
+                                                {
+                                                    type: 'point',
+                                                    xValue: '2021',
+                                                    yValue: 1500,
+                                                    backgroundColor: 'rgba(0, 255, 0, 0.5)',
+                                                    borderColor: 'rgb(0, 255, 0)',
+                                                    borderWidth: 2,
+                                                    radius: 5,
+                                                },
+                                                {
+                                                    type: 'point',
+                                                    xValue: '2023',
+                                                    yValue: 1200,
+                                                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                                                    borderColor: 'rgb(255, 0, 0)',
+                                                    borderWidth: 2,
+                                                    radius: 5,
+                                                }
+                                            ]
+                                        }
                                     }
                                 }
-                            }
-                        });
-                    </script>
+                            });
+
+                        </script>
+                    </div>
                 </div>
             </div>
         </div>
     </main>
     @endsection
-
 
 </body>
 
