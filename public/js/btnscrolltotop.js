e4314fca7dec444698593c7c452b5bbc48bafea4
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2024-06-07 05:45:33 +0530

    added btnscrolltotop

---
+++ public/js/btnscrolltotop.js
@@ -0,0 +1,27 @@
+// public/js/app.js or your main JS file
+    document.addEventListener("DOMContentLoaded", function() {
+        // Get the button
+        let mybutton = document.getElementById("scrollToTopBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.classList.remove('hidden');
+            } else {
+                mybutton.classList.add('hidden');
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document with smooth behavior
+        mybutton.addEventListener("click", function() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    });
+
