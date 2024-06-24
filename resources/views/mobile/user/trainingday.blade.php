22cfd60a0ff00d12eb6a18a2acb087ad6c05c265
Isuru Ekanayaka <133802550+isurulekanayaka@users.noreply.github.com>
2024-06-24 10:22:47 +0530

    Mobile UI add script

--- resources/views/mobile/user/trainingday.blade.php
+++ resources/views/mobile/user/trainingday.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -5,53 +6,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
     @extends('mobile.layout.mobile-layout')
 
     @section('content')
-        <div class="w-full flex flex-col justify-between min-h-screen h-full">
-            <div class="flex-grow items-center justify-center m-0 p-4 bg-cover bg-center bg-no-repeat"
-                style="background-image: url('{{ asset('img/valhalla-bg.jpg') }}');">
-                <div class="flex flex-col justify-center items-center gap-2.5 pt-32 text-white">
-                    <div
-                        class="day bg-black bg-transparent border w-72 border-white text-white p-5 rounded-lg  flex justify-between items-center transition duration-300 ease-in-out">
+    <div class="w-full flex flex-col justify-between min-h-screen h-full">
+        <div class="flex-grow items-center justify-center m-0 p-4 bg-cover bg-center bg-no-repeat"
+            style="background-image: url('{{ asset('img/valhalla-bg.jpg') }}');">
+            <div class="flex flex-col justify-center items-center gap-2.5 pt-32 text-white">
+
+                {{-- Monday Button  --}}
+                <button id="monday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white text-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
                         <span class="day-name text-lg font-bold">Monday</span>
-                        <span class="date text-sm">02/02/2024</span>
+                        <span class="date text-sm" id="monday-date"></span>
                     </div>
-                    <div
-                        class="day bg-black bg-transparent border w-72 border-white p-5 rounded-lg  flex justify-between items-center transition duration-300 ease-in-out">
+                </button>
+                {{-- Tuesday Button  --}}
+                <button id="tuesday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
                         <span class="day-name text-lg font-bold">Tuesday</span>
-                        <span class="date text-sm">02/02/2024</span>
+                        <span class="date text-sm" id="tuesday-date"></span>
                     </div>
-                    <div
-                        class="day bg-black bg-transparent border w-72 border-white p-5 rounded-lg  flex justify-between items-center transition duration-300 ease-in-out">
+                </button>
+                {{-- Wednesday Button  --}}
+                <button id="wednesday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
                         <span class="day-name text-lg font-bold">Wednesday</span>
-                        <span class="date text-sm">02/02/2024</span>
+                        <span class="date text-sm" id="wednesday-date"></span>
                     </div>
-                    <div
-                        class="day bg-black bg-transparent border w-72 border-white p-5 rounded-lg  flex justify-between items-center transition duration-300 ease-in-out">
+                </button>
+                {{-- Thursday Button  --}}
+                <button id="thursday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
                         <span class="day-name text-lg font-bold">Thursday</span>
-                        <span class="date text-sm">02/02/2024</span>
+                        <span class="date text-sm" id="thursday-date"></span>
                     </div>
-                    <div
-                        class="day bg-black bg-transparent border w-72 border-white p-5 rounded-lg  flex justify-between items-center transition duration-300 ease-in-out">
+                </button>
+                {{-- Friday Button  --}}
+                <button id="friday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
                         <span class="day-name text-lg font-bold">Friday</span>
-                        <span class="date text-sm">02/02/2024</span>
+                        <span class="date text-sm" id="friday-date"></span>
                     </div>
-                    <div
-                        class="day bg-black bg-transparent border w-72 border-white p-5 rounded-lg  flex justify-between items-center transition duration-300 ease-in-out">
+                </button>
+                {{-- Saturday Button  --}}
+                <button id="saturday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
                         <span class="day-name text-lg font-bold">Saturday</span>
-                        <span class="date text-sm">02/02/2024</span>
+                        <span class="date text-sm" id="saturday-date"></span>
                     </div>
-                </div>
+                </button>
+                {{-- Sunday Button  --}}
+                <button id="sunday-btn">
+                    <div class="day bg-transparent border w-72 border-black hover:border-white p-5 rounded-lg flex justify-between items-center transition duration-300 ease-in-out">
+                        <span class="day-name text-lg font-bold">Sunday</span>
+                        <span class="date text-sm" id="sunday-date"></span>
+                    </div>
+                </button>
+                
             </div>
-          
-        </div>
         </div>
     </div>
-    {{-- Local storage Date Set Script --}}
+    {{-- Local storage Date Set Script  --}}
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -92,6 +112,5 @@
 
     @endsection
 </body>
-
 
 </html>
